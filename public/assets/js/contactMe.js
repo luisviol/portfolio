7265726a7d3a514938aaada536e156c0d661d4cc
Luis Villasmil <luisviol22@gmail.com>
2024-05-02 15:07:53 -0400

    Font, Styling and portfolio detail edited

--- public/assets/js/contactMe.js
+++ public/assets/js/contactMe.js
@@ -6,8 +6,8 @@ class ContactMe extends HTMLElement {
     <section class="mt-4 mb-4 animate__animated animate__fadeInUp"><div class="card">
 
                <div class="card-body">
-                 <h5 class="card-title">Let's keep in touch</h5>
-                 <p class="card-text">At the moment I am only available for consulting, but I am always available to chat and share ideas.</p>
+                 <h5 class="card-title">Let's connect!</h5>
+                 <p class="card-text">Feel free to reach out anytime to chat and exchange ideas!</p>
                  <a href="mailto:luisviol22@gmail.com" class="btn btn-primary">Email me</a>
                </div>
              </div>
